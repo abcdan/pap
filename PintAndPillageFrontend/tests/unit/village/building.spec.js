4d9be3f7e34381a69367542cb0dc24e02a10e6d7
abcdan <daniel@lngzl.nl>
2022-09-20 10:25:01 +0200

    WIP

---
+++ PintAndPillageFrontend/tests/unit/village/building.spec.js
@@ -0,0 +1,69 @@
+import {createLocalVue, mount, shallowMount} from "@vue/test-utils";
+import Vuex from "vuex";
+import Farm from "@/components/tiles/Farm";
+import ConstructionTile from "@/components/tiles/ConstructionTile";
+
+let buildingWrapper;
+let store;
+let getters;
+const localVue = createLocalVue()
+localVue.component('construction-tile', ConstructionTile)
+localVue.use(Vuex)
+
+// beforeEach(() => {
+//
+// });
+
+function setup(level) {
+    getters = {
+        seasonsOn: () => {
+            return true
+        },
+        currentSeason: () => {
+            return 'winter'
+        },
+        buildingProperties: () => {
+            return {
+                level,
+                isUnderConstruction: false
+            }
+        }
+    }
+
+    const state = {
+        buildingProperties: {
+            level,
+            isUnderConstruction: false
+        }
+    }
+
+    store = new Vuex.Store({
+        state,
+        getters,
+    })
+
+    buildingWrapper = mount(Farm, {
+        store,
+        localVue,
+        propsData: {
+            buildingProperties: {
+                level,
+                isUnderConstruction: false
+            }
+        }
+    });
+}
+afterAll(() =>{
+    buildingWrapper.destroy()
+})
+
+describe('Building', () => {
+    it('should show visual change on level 10', async () => {
+        setup(1)
+
+        const nilsepils = buildingWrapper.findAll(".tileImg").at(0).html()
+        console.log(nilsepils)
+
+    })
+
+});
